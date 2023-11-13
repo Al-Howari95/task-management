7751a8311dd75d44466e36e7f8fadd3eff302bb0
Al-Howari95 <m.howari95@gmail.com>
2023-11-14 02:55:18 +0300

    test

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -3,13 +3,17 @@ import React from 'react';
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
 import "./App.css";
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div>
+      <Header/><br></br><br></br>
       <h1>Task Manager</h1>
       <TaskForm />
       <TaskList />
+      <Footer/>
     </div>
   );
 };
